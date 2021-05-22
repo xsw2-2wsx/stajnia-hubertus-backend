1bcc1ef37da6ab929fdf1821be463a5d5b761a5a
xsw2-2wsx <jan1undrych@gmail.com>
2021-05-22 15:04:14 +0200

    feat(project): add Junit5 dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,8 @@ dependencies {
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
+	testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
+	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 
 	configurations {
 		all {
