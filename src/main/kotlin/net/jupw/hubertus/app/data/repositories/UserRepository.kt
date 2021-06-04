32da49b7d622302c6c538cee99f97973d0582547
xsw2-2wsx <jan1undrych@gmail.com>
2021-06-04 12:48:53 +0200

    feat(UserRepository): add findByName method

--- src/main/kotlin/net/jupw/hubertus/app/data/repositories/UserRepository.kt
+++ src/main/kotlin/net/jupw/hubertus/app/data/repositories/UserRepository.kt
@@ -5,4 +5,6 @@ import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface UserRepository : CrudRepository<UserEntity, Int>
+interface UserRepository : CrudRepository<UserEntity, Int> {
+    fun findByName(name: String): UserEntity?
+}
