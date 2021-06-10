272ac7b968511d3b0076d7da303ac16e1a181a9a
xsw2-2wsx <jan1undrych@gmail.com>
2021-06-10 12:54:26 +0200

    refractor(app): remove id from ActivityConstraint app entity

--- src/main/kotlin/net/jupw/hubertus/app/entities/ActivityConstraint.kt
+++ src/main/kotlin/net/jupw/hubertus/app/entities/ActivityConstraint.kt
@@ -4,8 +4,6 @@ import java.time.LocalTime
 
 interface ActivityConstraint {
 
-    var id: Int
-
     var startTime: LocalTime
 
     var endTime: LocalTime
--- src/main/kotlin/net/jupw/hubertus/app/entities/ActivityConstraintImpl.kt
+++ src/main/kotlin/net/jupw/hubertus/app/entities/ActivityConstraintImpl.kt
@@ -3,7 +3,6 @@ package net.jupw.hubertus.app.entities
 import java.time.LocalTime
 
 class ActivityConstraintImpl(
-    override var id: Int,
     override var startTime: LocalTime,
     override var endTime: LocalTime,
 ) : ActivityConstraint
