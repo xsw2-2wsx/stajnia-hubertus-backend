5609a7906b4305c87a17c3b8ca16b06fa820ac4d
xsw2-2wsx <jan1undrych@gmail.com>
2021-06-12 17:28:15 +0200

    fix(AuthController): change controller annotation

--- src/main/kotlin/net/jupw/hubertus/api/controllers/AuthController.kt
+++ src/main/kotlin/net/jupw/hubertus/api/controllers/AuthController.kt
@@ -6,10 +6,10 @@ import net.jupw.hubertus.api.toResponseEntity
 import net.jupw.hubertus.app.interactor.AuthInteractor
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RestController
 
 
-@Controller
+@RestController
 class AuthController : AuthApi {
 
     @Autowired
