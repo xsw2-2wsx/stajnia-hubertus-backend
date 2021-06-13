baaf7a6ea468315a09869222f04c9c22965b81ba
xsw2-2wsx <jan1undrych@gmail.com>
2021-06-13 13:16:06 +0200

    feat(ConfigurationKey): add default value

--- src/main/kotlin/net/jupw/hubertus/app/configuration/ConfigurationKey.kt
+++ src/main/kotlin/net/jupw/hubertus/app/configuration/ConfigurationKey.kt
@@ -3,9 +3,11 @@ package net.jupw.hubertus.app.configuration
 import net.jupw.hubertus.util.Displayable
 
 sealed interface ConfigurationKey : Displayable {
+
     val key: String
 
-    fun validate(value: String)
+    val defaultValue: String
 
+    fun validate(value: String)
 
 }
