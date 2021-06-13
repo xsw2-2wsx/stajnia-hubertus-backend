02a9a4e380ced3bd5504b2f3b0b4fe459d01d028
xsw2-2wsx <jan1undrych@gmail.com>
2021-06-13 21:26:35 +0200

    feat(ConfKeys): implement business conf keys

--- src/main/kotlin/net/jupw/hubertus/app/configuration/ConfKeys.kt
+++ src/main/kotlin/net/jupw/hubertus/app/configuration/ConfKeys.kt
@@ -1,6 +1,39 @@
 package net.jupw.hubertus.app.configuration
 
+import net.jupw.hubertus.util.validation.and
+import net.jupw.hubertus.util.validation.validate
+
 enum class ConfKeys : ConfigurationKey {
+
+    MAX_POINTS {
+        override val displayName: String = "Maksymalna ilość punktów zajętości"
+
+        override val defaultValue: String = "6"
+
+        override val description: String = """
+            Zajętość obliczana jest poprzez zsumowanie punktów aktywności wszystkich rezerwacji, które są w danym
+            momencie aktywne. $displayName oznacza liczbę punktów która nie może zostać przekroczona. Jeśli będzie 
+            to konieczne, system odmówi użytkowniką składania nowych rezerwacji.
+        """.trimIndent()
+
+        override fun validate(value: String) = value.validate {
+            notBlank() and number() and min(0.0)
+        }
+
+    },
+    MIN_BOOKING_PRECEDENCE_HOURS {
+        override val displayName: String = "Minimalne wyprzedzenie rezerwacji"
+
+        override val defaultValue: String = "3600000"
+
+        override val description: String
+            get() = super.description + ". Wartość 0 wyłącza tą opcję"
+
+        override fun validate(value: String) = value.validate {
+            notBlank() and number() and min(0.0)
+        }
+
+    },
     ;
 
 
