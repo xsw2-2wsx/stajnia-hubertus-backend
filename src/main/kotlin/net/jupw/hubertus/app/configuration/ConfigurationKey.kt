915984c06a9c63a1c76d64830db47d43bf6a4178
xsw2-2wsx <jan1undrych@gmail.com>
2021-06-14 11:03:40 +0200

    fix(ConfigurationKey): remove sealed

--- src/main/kotlin/net/jupw/hubertus/app/configuration/ConfigurationKey.kt
+++ src/main/kotlin/net/jupw/hubertus/app/configuration/ConfigurationKey.kt
@@ -2,7 +2,7 @@ package net.jupw.hubertus.app.configuration
 
 import net.jupw.hubertus.util.Displayable
 
-sealed interface ConfigurationKey : Displayable {
+interface ConfigurationKey : Displayable {
 
     val key: String
 
