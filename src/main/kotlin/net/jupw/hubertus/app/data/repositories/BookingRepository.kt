f3a5c7c44b3341de603e601e4b9e7b1217367f4e
xsw2-2wsx <jan1undrych@gmail.com>
2021-07-11 14:36:04 +0200

    refractor(BookingRepository): use just one method for filtering

--- src/main/kotlin/net/jupw/hubertus/app/data/repositories/BookingRepository.kt
+++ src/main/kotlin/net/jupw/hubertus/app/data/repositories/BookingRepository.kt
@@ -9,16 +9,37 @@ import java.time.LocalDateTime
 @Repository
 interface BookingRepository : CrudRepository<BookingEntity, Int> {
 
-    @Query("SELECT * FROM bookings b WHERE b.users_id = :id", nativeQuery = true)
-    fun findAllByUserId(id: Int): Set<BookingEntity>
-
     @Query("""
         SELECT DISTINCT * 
         FROM bookings 
         WHERE
-            start_time > :start and start_time < :end or
-            end_time > :start and end_time < :end
+            (:rangeStart is null or :rangeEnd is null or (
+                (start_time > :rangeStart and start_time < :rangeEnd) or
+                (end_time > :rangeStart and end_time < :rangeEnd)
+            )) and
+            (:startTimeStart is null or start_time >= :startTimeStart) and
+            (:startTimeEnd is null or start_time <= :startTimeEnd) and
+            (:endTimeStart is null or end_time >= :endTimeStart) and
+            (:endTimeEnd is null or end_time <= :endTimeEnd) and
+            (:creationTimeEnd is null or creation_time >= :creationTimeStart) and
+            (:creationTimeEnd is null or creation_time <= :creationTimeEnd) and
+            (:userId is null or users_id = :userId) and
+            (:activityId is null or activities_id = :activityId) and
+            (:subject is null or subject = :subject)
+       
     """, nativeQuery = true)
-    fun findForRange(start: LocalDateTime, end: LocalDateTime): Set<BookingEntity>
+    fun filterBookings(
+        rangeStart: LocalDateTime? = null,
+        rangeEnd: LocalDateTime? = null,
+        startTimeStart: LocalDateTime? = null,
+        startTimeEnd: LocalDateTime? = null,
+        endTimeStart: LocalDateTime? = null,
+        endTimeEnd: LocalDateTime? = null,
+        creationTimeStart: LocalDateTime? = null,
+        creationTimeEnd: LocalDateTime? = null,
+        userId: Int? = null,
+        activityId: Int? = null,
+        subject: String? = null,
+    ): Set<BookingEntity>
 
 }
