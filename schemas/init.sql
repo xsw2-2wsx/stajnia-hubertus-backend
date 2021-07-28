ae6e3e031923cf67f0f8f09f67c92522477f31a0
xsw2-2wsx <jan1undrych@gmail.com>
2021-07-28 18:34:21 +0200

    feat(schemas): create initial data script

---
+++ schemas/init.sql
@@ -0,0 +1,23 @@
+insert into roles values
+    (1, 'Administrator', 'Wbudowana rola do zarządzania systemem'),
+    (2, 'Użytkownik', 'Uprawnienia dla standardowego użytkownika');
+
+insert into authorities values
+    ('MANAGE_ACTIVITIES', 1),
+    ('USE_BOOKING', 1),
+    ('MANAGE_BOOKINGS', 1),
+    ('MANAGE_ROLES', 1),
+    ('MANAGE_USERS', 1),
+    ('RESET_ANY_PASSWORD', 1),
+    ('USE_BOOKING', 2);
+
+insert into users values (
+    1,
+    'Administrator',
+    '{bcrypt}$2y$10$usBbY.FI5sSsGGkGFHbEeuI7edYKy9adWHGt4z3g.WP9nnKrAk4yW',
+    null,
+    null,
+    false
+);
+
+insert into users_roles values (1, 1);
