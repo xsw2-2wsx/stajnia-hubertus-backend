2286e6653c84b3f619c5c45c1e66b7499db959ee
xsw2-2wsx <jan1undrych@gmail.com>
2021-08-08 14:08:19 +0200

    feat(project): configure mail sending

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-log4j2")
+	implementation("org.springframework.boot:spring-boot-starter-mail")
 
 	implementation("io.jsonwebtoken:jjwt-api:0.11.1")
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.1")
